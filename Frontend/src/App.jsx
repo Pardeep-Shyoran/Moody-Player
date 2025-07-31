5f60272ddeedc8d61faa0829c77ad5424bfae621
Pardeep Shyoran <pardeep75889@gmail.com>
2025-07-31 07:10:33 +0530

    mood song component added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import FaceExpressionDetector from './components/FaceExpressionDetector';
+import FaceExpressionDetector from './components/FaceExpressionDetector/FaceExpressionDetector';
+import MoodSongs from './components/MoodSongs/MoodSongs';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Facial Expression Detection</h1>
+      <h1 className='Heading'>Facial Expression Detection</h1>
       <FaceExpressionDetector />
+      <MoodSongs />
     </div>
   );
 }
---
+++ Frontend/src/components/MoodSongs/MoodSongs.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import styles from "./MoodSongs.module.css"
+
+const MoodSongs = () => {
+
+    const [Songs, setSongs] = useState([
+        {
+            title: "test_song",
+            artist: "test_artist",
+            url: "test_url",
+        },
+        {
+            title: "test_song",
+            artist: "test_artist",
+            url: "test_url",
+        },
+        {
+            title: "test_song",
+            artist: "test_artist",
+            url: "test_url",
+        },
+    ]);
+
+
+    return (
+        <div className={styles.moodSongs}>
+            <h1 className={styles.songHead}>Recommended Songs</h1>
+            <div className={styles.songList}>
+                {Songs.map((song, index) => (
+                    <div key={index} className={styles.songBar}>
+                        <div className="title">
+                            <p>{song.title}</p>
+                            <p>{song.artist}</p>
+                        </div>
+                        <div className="play-pose-btn">
+                            <i class="ri-pause-circle-fill"></i>
+                            <i class="ri-play-circle-fill"></i>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default MoodSongs
